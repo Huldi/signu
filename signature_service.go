f81b5db8ad90966d96106805cef50ec2c53aea96
Kuzmin Dmitrii <d.kuzmin@iconic.vc>
2018-11-16 12:57:13 +0300

    .

--- signature_service.go
+++ signature_service.go
@@ -11,6 +11,7 @@ import "C"
 
 import (
     "unsafe"
+    "fmt"
 )
 
 const RESULT_SUCCESS uint = 0x00
@@ -49,6 +50,7 @@ func ETH_Adr(arr []byte) string{
 
 func Start_Wallet(){
     C.start_wallet()
+    fmt.Println("Да неужели?")
 }
 
 func Withdrawal_of_money(data []byte,len_data int, id []byte, sign *[]byte) bool{
